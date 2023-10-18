1ae3520cecea0921feec961b6b4b083b6ad79f4b
aldoademi <aldo.ademi011299@gmail.com>
2023-10-18 16:52:16 +0200

    Aggiunto controllo sul tempo

--- src/utilities/election_account_utilities.rs
+++ src/utilities/election_account_utilities.rs
@@ -1,11 +1,11 @@
-use chrono::{DateTime, Utc, Duration};
+use chrono::{NaiveDateTime, Utc, Duration};
 use solana_program::{entrypoint::ProgramResult, program_error::ProgramError};
 
 
 
 pub fn check_dates(
-    start_date: DateTime<Utc>,
-    end_date: DateTime<Utc>
+    start_date: NaiveDateTime,
+    end_date: NaiveDateTime
 ) -> ProgramResult {
 
     //creo data di oggi e aggiungo 20 min
@@ -20,6 +20,27 @@ pub fn check_dates(
         }
         
     } else {
+        return Err(ProgramError::InvalidInstructionData);
+    }
+}
+
+pub fn check_time_election(
+    start_date: NaiveDateTime,
+    end_date: NaiveDateTime
+) -> ProgramResult{
+    
+    //Creo data di oggi
+    let today = Utc::now();
+
+    //Verifico di essere all'interno del tempo di inizio e fine
+    if(today.unwrap() >= start_date){
+        // Verifico di essere ancora in tempo nell'eseguire le operazioni
+        if(end_date > today.unwrap()){
+            return Ok(());
+        }else{
+            return Err(ProgramError::InvalidInstructionData);
+        }
+    }else{
         return Err(ProgramError::InvalidInstructionData);
     }
 }
